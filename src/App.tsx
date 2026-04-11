22eec5ddafc47d04d5a0880dd52f597979357d31
anand17597 <60308472+anand17597@users.noreply.github.com>
2026-04-11 21:08:00 +0530

    Add src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import IndexPage from '@/pages/Index';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-light text-dark font-sans antialiased">
+      <IndexPage />
+    </div>
+  );
+}
+
+export default App;
